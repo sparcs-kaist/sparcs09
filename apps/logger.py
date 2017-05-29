25a86f6eb58cc21e6ecfd2ac260ed4665ab7cc3c
Tae-kyeom, Kim <kimtkyeom@kaist.ac.kr>
2017-05-29 23:15:04 +0900

    Fix logger kwargs type error (#24)

--- apps/logger.py
+++ apps/logger.py
@@ -29,5 +29,5 @@ class DBHandler(Handler):
 
         hide = record.args.get('hide', False)
         user = self.User.objects.filter(username=record.username).first()
-        self.UserLog(user=user, level=record.levelno, ip=record.ip, hide=hide,
+        self.UserLog(user=user, level=record.levelno, ip=record.ip, is_hidden=hide,
                      text='%s.%s' % (record.name, record.getMessage())).save()
