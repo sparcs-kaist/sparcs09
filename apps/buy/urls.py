7ffcb64e44ed447ec2665cd520aeb9352692eb19
Sam Jo <samjo.nyang@gmail.com>
2017-06-18 15:38:54 +0900

    reconstruct project directory (#28)
    * reconstruct project directory
    
    * remove all previous backend codes, statics and templates
    * upgrade django to 1.11.2
    * install django rest framework
    * add core and session apps
    * add .sqlite3 and archive/ to .gitignore
    
    * rename directory
    
    * sparcs09 -> sparcs09-api
    * vueapp -> sparcs09-web

--- apps/buy/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from apps.buy import views
-
-urlpatterns = [
-    url('^$', views.main),
-    url('^list/$', views.record),
-    url('^item/(?P<pid>\d+)/$', views.item),
-    url('^item/(?P<pid>\d+)/list/$', views.item_total),
-]
