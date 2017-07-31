f569fd60d183c4b44f53ce491f2a88cc1fe98095
Sam Jo <samjo.nyang@gmail.com>
2017-07-31 23:09:00 +0900

    implement sparcs sso unregister api (done #31)
    * implement sparcs sso unregister api (done #31)
    * add comments for the /users/unregister/ method

--- sparcs09-api/apps/session/viewsets/user.py
+++ sparcs09-api/apps/session/viewsets/user.py
@@ -1,17 +1,21 @@
 import logging
 
+from django.conf import settings
 from django.contrib.auth.models import User
 from rest_framework import viewsets
+from rest_framework.decorators import list_route
 from rest_framework.response import Response
 
 from apps.core.models import UserLog
 from apps.session.serializers import UserSerializer
+from apps.session.sparcsssov2 import Client
 
 
 logger = logging.getLogger(UserLog.GROUP_ACCOUNT)
+sso_client = Client(settings.SSO_ID, settings.SSO_KEY)
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ViewSet):
     lookup_field = 'sid'
     lookup_value_regex = '[0-9a-f]+'
 
@@ -87,7 +91,42 @@ class UserViewSet(viewsets.ModelViewSet):
         })
 
     # [DELETE] /users/<sid>/
-    def destroy():
+    def destroy(self, request, sid=None):
         return Response({
             'detail': 'Deleting user is not allowed.',
         }, status=403)
+
+    # [POST] /users/unregister/
+    @list_route(methods=['post'])
+    def unregister(self, request):
+        """
+        It follows SPARCS SSO unregister API specification
+        Do NOT change its url / request / response format
+        """
+        bad_response = Response({
+            'success': False,
+            'reason': 'Invalid request or no such user',
+        }, status=400)
+
+        try:
+            sid = sso_client.parse_unregister_request(request.POST)
+        except:
+            return bad_response
+
+        user = User.objects.filter(username=sid).first()
+        if not user:
+            return bad_response
+
+        # TODO: check this user can be unregistered or not
+        some_condition = True
+        if not some_condition:
+            return Response({
+                'success': False,
+                'reason': 'some-critical-reason',
+                'link': 'https://09.sparcs.org/some-policy-link',
+            })
+
+        user.delete()
+        return Response({
+            'success': True,
+        })
